0d1a7246fd843b7c4b937b672c25b488e6cab25e
binaryArrow <cancihan96@gmail.com>
2020-09-29 01:14:25 +0200

    Add files via upload

---
+++ heapsort.js
@@ -0,0 +1,52 @@
+"use strict"
+
+let array = [4,5,1,2,7,8,6,9,3];
+
+console.log(array);
+
+function heapsort(arr){
+    buildHeap(arr);
+    for(let i = arr.length -1; i >= 0; i--){
+        let temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+        console.log(array);
+        heapify(arr, i, 0);
+    }
+}
+
+function heapify(a, heapsize, i){
+    let l = 2*i+1;
+    let r = 2*i+2;
+    let max;
+
+    if(l < heapsize && a[l] > a[i]){
+        max = l;
+    }
+
+    else {
+        max = i;
+    }
+
+    if(r < heapsize && a[r] > a[max]){
+        max = r;
+    }
+
+    if(max != i){
+        let temp = a[i];
+        a[i] = a[max];
+        a[max] = temp;
+        heapify(a, heapsize, max);
+    }
+}
+
+
+function buildHeap(a){
+    for(let i = Math.floor(a.length/2); i >=0; i--){
+        heapify(a, a.length, i-1);
+    }
+}
+
+heapsort(array);
+
+console.log(array);
